7fe2d41d23f015308dca44384a0d145fecfdd74e
stryxsis <vladcostin58@gmail.com>
2025-01-16 18:46:05 +0100

    Aggiunta di taskList

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 class Task {
-  constructor(id, toDO, description, date, isComplited) {
+  constructor(typeOfTask, id, toDO, description, date, isComplited) {
+    this._typeOfTask = typeOfTask;
     this._id = id;
     this._toDO = toDO;
     this._description = description;
@@ -7,8 +8,32 @@ class Task {
     this._isComplited = isComplited;
   }
 
-  get editTask() {
-    this._toDO, this._description, this._date;
+  get typeOfTask() {
+    return this._typeOfTask;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get toDO() {
+    return this._toDO;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  get date() {
+    return this._date;
+  }
+
+  get isComplited() {
+    return this._isComplited;
+  }
+
+  set typeOfTask(typeOfTask) {
+    this._typeOfTask = typeOfTask;
   }
 
   set toDO(toDO) {
@@ -23,7 +48,7 @@ class Task {
     this._date = date;
   }
 
-  isComplited() {
+  changeIsComplited() {
     this._isComplited = !this._isComplited;
   }
 
@@ -94,7 +119,8 @@ class Task {
 }
 
 class TaskList {
-  constructor(id, toDO, descriptionList, date, isComplited) {
+  constructor(typeOfTask, id, toDO, descriptionList, date, isComplited) {
+    this._typeOfTask = typeOfTask;
     this._id = id;
     this._toDO = toDO;
     this._descriptionList = descriptionList;
@@ -102,24 +128,151 @@ class TaskList {
     this._isComplited = isComplited;
   }
 
-  get editTask() {
-    return this._toDO, this._descriptionList, this._date;
+  get typeOfTask() {
+    return this._typeOfTask;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get toDO() {
+    return this._toDO;
+  }
+
+  get descriptionList() {
+    return this._descriptionList;
+  }
+
+  get date() {
+    return this._date;
+  }
+
+  get isComplited() {
+    return this._isComplited;
+  }
+
+  set typeOfTask(typeOfTask) {
+    this._typeOfTask = typeOfTask;
   }
 
   set toDO(toDO) {
     this._toDO = toDO;
   }
 
-  set description(descriptionList) {
-    this._description = descriptionList;
+  set descriptionList(descriptionList) {
+    this._descriptionList = descriptionList;
   }
 
   set date(date) {
     this._date = date;
   }
 
-  isComplited() {
+  set isCompleted(isCompleted) {
+    this._isComplited = isCompleted;
+  }
+
+  changeIsComplited() {
     this._isComplited = !this._isComplited;
+  }
+
+  changeTaskIsComplited(indexTask) {
+    this._descriptionList[indexTask].isComplited = !this._descriptionList[indexTask].isComplited;
+  }
+
+  changeTaskIsComplitedAllTrue(indexTask) {
+    this._descriptionList[indexTask].isComplited = true;
+  }
+
+  renderingDescriptionList() {
+    const descriptionListHTML = this._descriptionList
+      .map((item) => {
+        return `
+        <div class="checkbox-wrapper-4">
+          <input class="inp-cbx" id="${item.id}" type="checkbox" onclick="checkListTask(this)" ${item.isComplited ? "checked" : ""}/>
+          <label class="cbx" for="${item.id}">
+            <span>
+              <svg width="12px" height="10px"></svg>
+            </span>
+            <span class="${item.isComplited ? "task-list-comlited" : ""}">${item.item}</span>
+          </label>
+          <svg class="inline-svg">
+          <symbol id="check-4" viewBox="0 0 12 10">
+            <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+          </symbol>
+        </svg>
+        </div>
+      `;
+      })
+      .join("");
+    return descriptionListHTML;
+  }
+
+  renderingHTML() {
+    return `
+    <li id="${this._id}" class="task ${this._isComplited ? "task-complited" : ""}">
+      <section class="container-check-box">
+        <div class="checkbox-wrapper-15">
+          <input onclick="checkTask(this)" class="inp-cbx" id="cbx-${this._id}" type="checkbox" style="display: none" ${this._isComplited ? "checked" : ""}/>
+          <label class="cbx" for="cbx-${this._id}">
+            <span>
+              <svg width="12px" height="9px" viewbox="0 0 12 9">
+                <polyline points="1 5 4 8 11 1"></polyline>
+              </svg>
+            </span>
+          </label>
+        </div>
+      </section>
+      <section class="container-task">
+        <div class="container-ToDo">
+          <h3>${this._toDO}</h3>
+        </div>
+        ${
+          this._descriptionList || this._date
+            ? `
+            <div class="main-task">
+              ${
+                this._descriptionList
+                  ? `
+                  <div class="container-description">
+                    <ul id="descriptionList-${this._id}">
+                      ${this.renderingDescriptionList()}
+                    </ul>
+                  </div>
+                  `
+                  : ""
+              }
+              ${
+                this._date
+                  ? `
+                  <div class="container-date">
+                    <span>${this._date}</span>
+                  </div>
+                  `
+                  : ""
+              }
+            </div>
+          `
+            : ""
+        }
+      </section>
+      <section class="container-btn-ico">
+      <button class="btn-ico" type="button" onclick="editTask(this)" aria-label="edit task button">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil">
+          <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+          <path d="m15 5 4 4" />
+        </svg>
+      </button>
+      <button class="btn-ico" type="button" onclick="removeTask(this)" aria-label="delet task button">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash">
+          <path d="M3 6h18" />
+          <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+          <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+        </svg>
+      </button>
+    </section>
+    </li>
+    `;
   }
 }
 
@@ -138,22 +291,17 @@ const inputDeadline = document.getElementById("input-deadline");
 const containerTask = document.getElementById("container-task");
 const counterTaskToDo = document.getElementById("counter-task-ToDo");
 let allTask = [];
-let currentTask = {
-  id: null,
-  toDO: "",
-  description: "",
-  date: "",
-  isComplited: false,
-};
+let currentTaskId = undefined;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const savedTheme = localStorage.getItem("theme");
+  const savedTheme = localStorage.getItem("theme") || "dark";
+  document.documentElement.setAttribute("data-theme", savedTheme);
   if (savedTheme) {
-    document.documentElement.setAttribute("data-theme", savedTheme);
+    switchDarkLight.checked = savedTheme === "dark";
   }
 
-  allTask = JSON.parse(localStorage.getItem("allTask")) || [];
-  tasksDisplay();
+  allTask = loadTaskFromLocalStorage() || [];
+  if (allTask) tasksDisplay();
 });
 
 switchDarkLight.addEventListener("click", () => {
@@ -166,6 +314,7 @@ switchDarkLight.addEventListener("click", () => {
 
 btnOppenModal.addEventListener("click", () => {
   btnModalSubmit.textContent = "Add Todo";
+  btnModalSubmit.setAttribute("data-action", "add");
   taskModal.classList.toggle("modal-overley-view");
 });
 
@@ -189,30 +338,57 @@ modulo.addEventListener("submit", (e) => {
   pickTask();
 });
 
-// Takes the values ​​of the inputs
-// const pickTask = () => {
-//   currentTask.toDO = inputTask.value.trim();
-//   currentTask.description = inputDescription.value.trim();
-//   currentTask.date = inputDeadline.value;
+const createDescriptionList = (input) => {
+  return input
+    .split(",")
+    .map((item) => item.trim())
+    .filter((item) => item !== "")
+    .map((item) => ({
+      id: `${item}-${Date.now()}`,
+      item: item,
+      isComplited: false,
+    }));
+};
 
-//   if (btnModalSubmit.textContent === "Edit task") {
-//     const taskEditIndex = allTask.findIndex((item) => item.id === currentTask.id);
-//     allTask[taskEditIndex] = { ...currentTask };
-//   } else {
-//     currentTask.id = `${Date.now()}`;
-//     allTask.unshift({ ...currentTask });
-//   }
+const updateTask = (task, updates) => {
+  task.typeOfTask = updates.typeOfTask;
+  task.toDO = updates.toDO;
+  task.description = updates.description;
+  task.descriptionList = updates.descriptionList || [];
+  task.date = updates.date;
+};
 
-//   resetModalInput();
-//   tasksDisplay();
-// };
+const createTaskObject = (type, id, toDO, description, descriptionList, date, isCompleted) => {
+  if (type === "taskList") {
+    return new TaskList(type, id, toDO, descriptionList, date, isCompleted);
+  } else {
+    return new Task(type, id, toDO, description, date, isCompleted);
+  }
+};
 
 const pickTask = () => {
-  if (btnDescriptionList.checked) {
-    //TODO: Implementare il taskList
-    null;
+  const action = btnModalSubmit.getAttribute("data-action");
+  const descriptionTaskObj = btnDescriptionList.checked ? createDescriptionList(inputDescription.value) : null;
+
+  if (action === "edit") {
+    const taskEdit = allTask.find((item) => item.id === currentTaskId);
+    const taskEditIndex = allTask.findIndex((item) => item.id === currentTaskId);
+    if (taskEdit) {
+      updateTask(taskEdit, {
+        toDO: inputTask.value.trim(),
+        description: btnDescriptionList.checked ? null : inputDescription.value.trim(),
+        descriptionList: descriptionTaskObj,
+        date: inputDeadline.value,
+      });
+      if ((btnDescriptionList.checked ? "taskList" : "task") !== taskEdit.typeOfTask) {
+        const newTask = createTaskObject(btnDescriptionList.checked ? "taskList" : "task", `${Date.now()}`, inputTask.value.trim(), btnDescriptionList.checked ? null : inputDescription.value.trim(), descriptionTaskObj, inputDeadline.value || "", false);
+        allTask[taskEditIndex] = newTask;
+      }
+    } else {
+      console.error("Errore: Task da modificare non trovato.");
+    }
   } else {
-    const newTask = new Task(`${Date.now()}`, inputTask.value.trim(), inputDescription.value.trim() || "", inputDeadline.value || "", false);
+    const newTask = createTaskObject(btnDescriptionList.checked ? "taskList" : "task", `${Date.now()}`, inputTask.value.trim(), btnDescriptionList.checked ? null : inputDescription.value.trim(), descriptionTaskObj, inputDeadline.value || "", false);
     allTask.unshift(newTask);
   }
 
@@ -225,11 +401,7 @@ const resetModalInput = () => {
   inputDescription.value = "";
   inputDeadline.value = "";
 
-  currentTask.id = null;
-  currentTask.toDO = "";
-  currentTask.description = undefined;
-  currentTask.date = "";
-  currentTask.isComplited = false;
+  currentTaskId = undefined;
 
   taskModal.classList.toggle("modal-overley-view");
 };
@@ -249,20 +421,70 @@ const removeTask = (task) => {
 };
 
 const editTask = (task) => {
-  const taskEditIndex = allTask.findIndex((item) => item.id === task.parentElement.parentElement.id);
-  currentTask = { ...allTask[taskEditIndex] };
+  const taskEdit = allTask.find((item) => item.id === task.parentElement.parentElement.id);
+  currentTaskId = task.parentElement.parentElement.id;
 
-  inputTask.value = currentTask.toDO;
-  inputDescription.value = currentTask.description;
-  inputDeadline.value = currentTask.date;
+  inputTask.value = taskEdit.toDO;
+  inputDeadline.value = taskEdit.date;
+  if (taskEdit.typeOfTask === "task") {
+    inputDescription.value = taskEdit.description;
+  } else if (taskEdit.typeOfTask === "taskList") {
+    taskEdit.descriptionList.forEach((item, index) => {
+      inputDescription.value += item.item;
+      if (index < taskEdit.descriptionList.length - 1) {
+        inputDescription.value += ", ";
+      }
+    });
+    btnDescriptionList.checked = true;
+  }
   btnModalSubmit.textContent = "Edit task";
+  btnModalSubmit.setAttribute("data-action", "edit");
   taskModal.classList.toggle("modal-overley-view");
 };
 
 const checkTask = (task) => {
+  const id = task.parentElement.parentElement.parentElement.id;
   const checkTaskIndex = allTask.findIndex((item) => item.id === task.parentElement.parentElement.parentElement.id);
-  allTask[checkTaskIndex].isComplited = !allTask[checkTaskIndex].isComplited;
+  // const checkTask = allTask.find((item) => item.id === task.parentElement.parentElement.parentElement.id);
+  allTask[checkTaskIndex].changeIsComplited();
+
+  allTask[checkTaskIndex].descriptionList.forEach((item, index) => {
+    allTask[checkTaskIndex].changeTaskIsComplitedAllTrue(index);
+  });
+  children = document.querySelectorAll(`#descriptionList-${id} > div`);
+  children.forEach((item) => {
+    item.children[0].checked = true;
+    item.children[1].children[1].classList.add("task-list-comlited");
+  });
   task.parentElement.parentElement.parentElement.classList.toggle("task-complited");
+  countTaskToDo();
+  saveLocalData();
+};
+
+const checkListTask = (task) => {
+  const taskIndex = allTask.findIndex((item) => item.id === task.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.id);
+  const checkListTaskIndex = allTask[taskIndex].descriptionList.findIndex((item) => item.id === task.id);
+  const pederCheckbox = document.querySelector(`#cbx-${allTask[taskIndex].id}`);
+  allTask[taskIndex].changeTaskIsComplited(checkListTaskIndex);
+  task.nextElementSibling.children[1].classList.toggle("task-list-comlited");
+  let counterTrue = 0;
+  allTask[taskIndex].descriptionList.forEach((item) => {
+    if (item.isComplited) {
+      counterTrue++;
+    }
+    if (counterTrue === allTask[taskIndex].descriptionList.length) {
+      checkTask(pederCheckbox);
+      pederCheckbox.checked = true;
+    } else {
+      pederCheckbox.checked = false;
+      allTask[taskIndex].isCompleted = false;
+      document.getElementById(`${allTask[taskIndex].id}`).classList.remove("task-complited");
+
+      if (allTask[taskIndex].isComplited) {
+        checkTask(pederCheckbox);
+      }
+    }
+  });
   countTaskToDo();
   saveLocalData();
 };
@@ -273,6 +495,23 @@ const countTaskToDo = () => {
     item.isComplited ? counter++ : null;
   });
   counterTaskToDo.textContent = allTask.length - counter;
+};
+
+const loadTaskFromLocalStorage = () => {
+  const tasks = JSON.parse(localStorage.getItem("allTask")) || [];
+  tasks
+    .map((task) => {
+      switch (task._typeOfTask) {
+        case "task":
+          return new Task(task._typeOfTask, task._id, task._toDO, task._description, task._date, task._isComplited);
+        case "taskList":
+          return new TaskList(task._typeOfTask, task._id, task._toDO, task._descriptionList, task._date, task._isComplited);
+        default:
+          console.warn("Tipo di task sconosciuto:", task);
+          return null;
+      }
+    })
+    .filter(Boolean);
 };
 
 const saveLocalData = () => {
